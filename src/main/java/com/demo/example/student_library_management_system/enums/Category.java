087897583d04a9d7a72c072d5940546e6ee30f1c
Chandan K M <chandanchintu16@gmail.com>
2025-04-29 22:32:20 +0530

    Model and relationship are created.

---
+++ src/main/java/com/demo/example/student_library_management_system/enums/Category.java
@@ -0,0 +1,14 @@
+package com.demo.example.student_library_management_system.enums;
+
+public enum Category {
+    // enum - it is pre-defined data type which contains constants for the specific fields
+
+    DRAMA,
+    MYTHOLOGY,
+    COMEDY,
+    HORROR,
+    TECHNICAL,
+    NON_TECHNICAL,
+    THRILLER,
+    FICTION
+}
