4aa3a54cf05f88261c2d4b9f41520d2888380853
Chandan K M <chandanchintu16@gmail.com>
2025-05-03 13:17:07 +0530

    Pagination +Sorting +exception handling

--- src/main/java/com/demo/example/student_library_management_system/service/StudentService.java
+++ src/main/java/com/demo/example/student_library_management_system/service/StudentService.java
@@ -6,7 +6,10 @@ import com.demo.example.student_library_management_system.model.Card;
 import com.demo.example.student_library_management_system.model.Student;
 import com.demo.example.student_library_management_system.repository.StudentRepository;
 import com.demo.example.student_library_management_system.requestdto.StudentRequestDto;
+import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,13 +42,50 @@ public class StudentService {
         Optional<Student> studentOptional = studentRepository.findById(id);
         if(studentOptional.isPresent()){
             return studentOptional.get();
+        }else{
+            throw new RuntimeException("Student not found : "+id);
         }
-        return null;
     }
 
     public List<Student> getAllStudents(){
         List<Student> studentList = studentRepository.findAll();
         return studentList;
+    }
+
+    /*
+    pagination - fetching or getting the records or data in the form of pages
+    pagenumber - the number of page we want to see(0,1,2,3,4,5,....)
+    pagesize - total number of records in each page(fixed for all pages)
+
+    total number of records - 28, page size - 5
+    0th page - 1-5
+    1st page - 6-10
+    2nd page - 11-15
+    3rd page - 16-20
+    4th page - 21-25
+    5th page - 26-28
+    6th page - 0
+
+     total number of records - 11, page size - 3
+     0th page -1-3
+     1st page - 4-6
+     2nd page - 7-9
+     3rd page - 10-11
+
+     sorting - arranging the record based on ascending or descending order
+
+     only pagination
+     public List<Student> getAllStudentsByPage(int pageNo, int pageSize){
+       List<Student> studentList = studentRepository.findAll(PageRequest.of(pageNo, pageSize)).getContent();
+       return studentList;
+    }
+
+     */
+
+    // pagination and sorting together
+    public List<Student> getAllStudentsByPage(int pageNo, int pageSize){
+       List<Student> studentList = studentRepository.findAll(PageRequest.of(pageNo, pageSize, Sort.by("name").ascending())).getContent();
+       return studentList;
     }
 
     public String deletedStudentById(int id){
